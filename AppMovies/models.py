0b2b2b9f800da3ee024cccae00eca1db163ea9cd
riyadhswe <riyadhswe@gmail.com>
2022-04-10 23:40:37 +0600

    drf

--- AppMovies/models.py
+++ AppMovies/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 
 # Create your models here.
-class Moviedata(models.Model):
+class MovieData(models.Model):
     name = models.CharField(max_length=200)
     duration = models.FloatField()
     rating = models.FloatField()
+    typ = models.CharField(max_length=200,default='action')
+    image = models.ImageField(upload_to='Image/',default='Images/None/Noimg.jpg')
 
     def __str__(self):
         return self.name
